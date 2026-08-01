594f025e1642309b1574c417849ebe5861afbf29
Trottinette électrique <clementfael@icloud.com>
2026-08-01 06:56:04 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,14 @@
+function searchProduct() {
+    let input = document.getElementById("search").value.toLowerCase();
+    let cards = document.querySelectorAll(".card");
 
+    cards.forEach(function(card) {
+        let title = card.querySelector("h3").textContent.toLowerCase();
+
+        if (title.includes(input)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
+}
